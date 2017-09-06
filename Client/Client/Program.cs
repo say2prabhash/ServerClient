e8210f291426cc4cadec7d8c78f69751ff54fc6d
Prabhash <pmohanty@tavisca.com>
2017-09-06 20:07:05 +0530

    Files added

---
+++ Client/Client/Program.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("prabhash");
+            IPAddress ipAddress = IPAddress.Parse("172.16.14.141");
+            TcpClient tcpclnt = new TcpClient();
+            tcpclnt.Connect(ipAddress, 8080);
+            NetworkStream stream = tcpclnt.GetStream();
+            string output = JsonConvert.SerializeObject(person);
+            byte[] b = ASCIIEncoding.ASCII.GetBytes(output);
+            stream.Write(b, 0, b.Length);
+            stream.Close();
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        const int PortNum = 8080;
+        const string ServerIP = "172.16.14.122";
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse(ServerIP);
+            TcpListener listener = new TcpListener(ip, PortNum);
+            Console.WriteLine("Listening...");
+            listener.Start();
+            TcpClient client = listener.AcceptTcpClient();
+            NetworkStream nwStream = client.GetStream();
+            byte[] buffer = new byte[client.ReceiveBufferSize];
+            int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+            string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            var person =Deserialize<Person>(dataReceived);
+            Console.WriteLine("Received : " + person.Name);
+            Console.Read();
+        }
+        public static T Deserialize<T>(string obj) where T : Person
+        {
+            return JsonConvert.DeserializeObject<T>(obj);
+        }
+    }
+}
